cde64e10005688e2735c82847efe74f51a13e996
Suman Bhattarai <suman.bhattarai@gmail.com>
2025-11-04 21:21:41 +0545

    backup config changes

--- routes/console.php
+++ routes/console.php
@@ -5,4 +5,4 @@ declare(strict_types=1);
 use Illuminate\Support\Facades\Schedule;
 
 Schedule::command('backup:clean')->daily()->at('01:00');
-Schedule::command('backup:run')->daily()->at('01:30');
+Schedule::command('backup:run --only-db --disable-notifications')->daily()->at('01:30');
